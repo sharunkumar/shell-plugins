a9005e99d5f126157ec441b4b747c316ea485a55
Sharun <sharunksplus@gmail.com>
2023-06-05 02:15:57 -0400

    updated test

--- plugins/skeet/app_password_test.go
+++ plugins/skeet/app_password_test.go
@@ -2,21 +2,21 @@ package skeet
 
 import (
 	"testing"
-	
+
 	"github.com/1Password/shell-plugins/sdk"
 	"github.com/1Password/shell-plugins/sdk/plugintest"
 	"github.com/1Password/shell-plugins/sdk/schema/fieldname"
 )
-	
+
 func TestAppPasswordProvisioner(t *testing.T) {
 	plugintest.TestProvisioner(t, AppPassword().DefaultProvisioner, map[string]plugintest.ProvisionCase{
 		"default": {
 			ItemFields: map[sdk.FieldName]string{ // TODO: Check if this is correct
-				fieldname.: "kazhj4oy2wjfexample",
+				fieldname.Username: "kazhj4oy2wjfexample",
 			},
 			ExpectedOutput: sdk.ProvisionOutput{
 				Environment: map[string]string{
-					"SKEET": "kazhj4oy2wjfexample",
+					"SKEET_USERNAME": "kazhj4oy2wjfexample",
 				},
 			},
 		},
@@ -27,12 +27,12 @@ func TestAppPasswordImporter(t *testing.T) {
 	plugintest.TestImporter(t, AppPassword().Importer, map[string]plugintest.ImportCase{
 		"environment": {
 			Environment: map[string]string{ // TODO: Check if this is correct
-				"SKEET": "kazhj4oy2wjfexample",
+				"SKEET_USERNAME": "kazhj4oy2wjfexample",
 			},
 			ExpectedCandidates: []sdk.ImportCandidate{
 				{
 					Fields: map[sdk.FieldName]string{
-						fieldname.: "kazhj4oy2wjfexample",
+						fieldname.Username: "kazhj4oy2wjfexample",
 					},
 				},
 			},
@@ -44,11 +44,11 @@ func TestAppPasswordImporter(t *testing.T) {
 				// "~/path/to/config.yml": plugintest.LoadFixture(t, "config.yml"),
 			},
 			ExpectedCandidates: []sdk.ImportCandidate{
-			// 	{
-			// 		Fields: map[sdk.FieldName]string{
-			// 			fieldname.Token: "kazhj4oy2wjfexample",
-			// 		},
-			// 	},
+				// 	{
+				// 		Fields: map[sdk.FieldName]string{
+				// 			fieldname.Token: "kazhj4oy2wjfexample",
+				// 		},
+				// 	},
 			},
 		},
 	})
